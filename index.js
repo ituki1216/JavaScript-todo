26b13be93140b2367c95b3a60ee382811d9a9dc2
ituki yamanaka <yamanakasan@icloud.com>
2024-10-26 16:34:01 +0900

    from, input, ulタグの取得

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 // htmlのfromタグ取得する
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const ul = document.getElementBuId("ul") // htmlからulタグを取得する
+const ul = document.getElementById("ul") // htmlからulタグを取得する
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     add();
-})  
+});
 
 function add() {
     const li = document.createElement("li"); // liタグを作るのでdocument.createElementでliを指定する
@@ -16,4 +16,4 @@ function add() {
     li.classList.add("list-group-item"); // liタグにデザインを適用したいのでclassリストにlist-group-itemをhtml側に追加する
     ul.appendChild(li);
     input.value = ""; // 入力フォームを送信する毎に空にする
-}
+};
