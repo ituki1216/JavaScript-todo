3cc1c6a44e1ede7f56be571a65f341336cda76ef
ituki yamanaka <yamanakasan@icloud.com>
2024-10-26 16:24:11 +0900

    add関数を定義

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //現在時刻は2024/10/26です。眠いです。
-console.log("hello");
 
 // htmlのfromタグ取得する
-const form = document.getElementById("form")
-const input = document.getElementById("input")
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementBuId("ul") // htmlからulタグを取得する
 
-form.addEventlistener("submit", function () {
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    add();
+})  
 
-})
+function add() {
+    const li = document.createElement("li"); // liタグを作るのでdocument.createElementでliを指定する
+    li.innerText = input.value; // ユーザーが入力した値を取得したいのでuserが入力するinput valueとする
+    li.classList.add("list-group-item"); // liタグにデザインを適用したいのでclassリストにlist-group-itemをhtml側に追加する
+    ul.appendChild(li);
+    input.value = ""; // 入力フォームを送信する毎に空にする
+}
