f43cf74e1b2ae8ea37ba16776caf5d19ca59372b
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-27 21:25:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")  # HTMLファイルを返す
+
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.get_json()  # フロントからの送信情報をJSON形式で取得する
+    result = {'message': 'データを処理しました！'}
+    return jsonify(result)
